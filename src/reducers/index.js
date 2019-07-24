fd2066d01be28c0c25e190ba578870ebd27f6f66
sumil <sumilprincejain@gmail.com>
2019-07-24 23:51:08 +0530

    adding active book reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
-import bookList from './reducers_bookList'
+import bookList from './reducers_bookList';
+import activeBook from './reducers_active_book';
 
 const rootReducer = combineReducers({
-  books: bookList
+  books: bookList,
+  activeBook : activeBook
 });
 
 export default rootReducer;
