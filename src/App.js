097126491fb28e7b720c8e1c502143459b8dfbcd
sumil <sumilprincejain@gmail.com>
2019-07-25 23:29:51 +0530

    creating book detail

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import BookList from './components/BookList';
+import BookDetail from './components/BookDetail';
 
 function App() {
   return (
     <div>
       <BookList />
+      <BookDetail />
     </div>
   );
 }
